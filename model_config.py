19c29a0dd16022a610020600dc55cadef3aa5876
Gili Gordiyenko <60244395+ggordi@users.noreply.github.com>
2025-06-13 10:00:17 -0400

    Update model_config.py

---
+++ model_config.py
@@ -0,0 +1,29 @@
+import pandas as pd 
+from sklearn.neighbors import NearestNeighbors
+import joblib
+import json
+
+# load the trained model
+knn = joblib.load('model.joblib')
+model_data = pd.read_csv("encoded_model.csv")
+model_data = model_data.set_index('Ticker')
+feature_cols = model_data.columns
+
+# user input should be in a dictionary format: 
+# {
+#    'sector': 'Healthcare',
+#    'marketCapLevel': 'high',
+#    'growthValueType': 'growth',
+#    'forwardPE': 15
+# }
+def get_recs(user_input):
+    # encode user input
+    user_df = pd.DataFrame([user_input])
+    user_encoded = pd.get_dummies(user_df)
+    user_encoded = user_encoded.reindex(columns=feature_cols, fill_value=0)
+
+    # perform nn search
+    distances, indices = knn.kneighbors(user_encoded.values)
+    nearest_tickers = model_data.iloc[indices[0]].index.tolist()
+
+    return json.dumps({'recs': nearest_tickers})
